9eb4e97ac10f460011ae12d7ebb471b8940a2182
JorgeTejel <jorgetejelcatalan@gmail.com>
2023-05-18 18:25:33 +0200

    Commit 18:25
    Algunos cambios pero sigue funcionando

--- resources/views/Componentes/index.blade.php
+++ resources/views/Componentes/index.blade.php
@@ -6,7 +6,7 @@
 
 <main>
     <div class="container py-4">
-        <h2>Listado de Componentes</h2>
+        <h2>Stock de Componentes</h2>
 
 
         <a href="{{url('componentes/create')}}" class="btn btn-primary btn-sm">Nuevo Registro</a>
