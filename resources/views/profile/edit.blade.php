24139bbad65296030e880563ccfe4faef928a084
JorgeTejel <jorgetejelcatalan@gmail.com>
2023-05-29 00:49:08 +0200

    Fix: Ingles a Español
    - Cambiando vistas que tenia en ingles a español.

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -13,10 +13,13 @@
     }
 </style>
 
+<title>Rider's Hub</title>
+<link rel="icon" type="image/pg" href="{{ asset('img/logoRH.png') }}">
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+            {{ __('Perfil') }}
         </h2>
     </x-slot>
 
